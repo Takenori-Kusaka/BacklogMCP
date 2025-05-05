d1c4dbedd35611c8dbf1db3343c131c47e74a5ed
Takenori Kusaka <kokorokagami@gmail.com>
2025-05-05 16:12:09 +0900

    Refactor CI configuration for Python tests and enhance Jest configuration for coverage reporting

--- cdk/jest.config.js
+++ cdk/jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
 };
